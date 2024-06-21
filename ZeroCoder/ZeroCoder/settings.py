493aae67a2b6aa86f8fc43df14be9022cb9e3a1c
TvorezReal <sentr777@bk.ru>
2024-06-21 14:57:31 +0300

    add new code

--- ZeroCoder/ZeroCoder/settings.py
+++ ZeroCoder/ZeroCoder/settings.py
@@ -21,6 +21,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'news',
     'main',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -94,7 +95,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
